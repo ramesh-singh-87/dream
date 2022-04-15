677c4bc032ced152063fff19c9d0d2c7bfeb57c6
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-04-15 07:54:45 -0500

    Implemented bfs and calculation of shortest path in Adjacency Matrix.

--- data-structures/graphs/src/main/java/com/alphacoder/AdjacencyMatrix.java
+++ data-structures/graphs/src/main/java/com/alphacoder/AdjacencyMatrix.java
@@ -1,13 +1,18 @@
 package com.alphacoder;
 
+import java.util.*;
+
 public class AdjacencyMatrix {
 
     private int vertices;
     private int [][] matrix;
+    private int [] parent;
+
 
     public AdjacencyMatrix(int vertices){
         this.vertices= vertices;
         matrix= new int[vertices][vertices];
+        parent= new int[vertices];
     }
 
     /*
@@ -53,5 +58,50 @@ public class AdjacencyMatrix {
             System.out.println();
         }
     }
+
+    /*
+    Time complexity: O(V2)
+     */
+    public void bfs(int start) {
+        Queue<Integer> vertexQueue = new LinkedList<>();
+        boolean[] visited = new boolean[vertices];
+        vertexQueue.offer(start);
+        visited[start] = true;
+        parent[start] = -1;
+
+        while (!vertexQueue.isEmpty()) {
+            int vertex = vertexQueue.poll();
+            System.out.print(vertex + " ");
+            for (int i = 0; i < vertices; i++) {
+                if (matrix[vertex][i] == 1 && !visited[i]) {
+                    vertexQueue.offer(i);
+                    visited[i] = true;
+                    parent[i] = vertex;
+                }
+            }
+        }
+    }
+
+    public int shortestPath(int source, int destination){
+        if(source== destination){
+            return 0;
+        }
+        int shortestPath= 1;
+        int temp= destination;
+        List<Integer> path= new ArrayList<>();
+        path.add(0, destination);
+        while(parent[temp]!= source){
+            shortestPath++;
+            temp= parent[temp];
+            if(temp!=source) {
+                path.add(0, temp);
+            }
+        }
+        path.add(0, source);
+        path.stream().forEach(element -> System.out.print(element+ " "));
+
+        return shortestPath;
+    }
+
 
 }
--- data-structures/graphs/src/main/java/com/alphacoder/AppMain.java
+++ data-structures/graphs/src/main/java/com/alphacoder/AppMain.java
@@ -12,6 +12,14 @@ public class AppMain {
         graph.addEdge(3, 4);
         graph.printGraph();
         graph.deleteEdge(0, 4);
+        graph.deleteEdge(1, 4);
+        graph.deleteEdge(1, 3);
         graph.printGraph();
+
+        //adjacencyMatrix bfs
+        graph.bfs(0);
+        System.out.println();
+
+        System.out.println("Shortest path: "+graph.shortestPath(0,4));
     }
 }
