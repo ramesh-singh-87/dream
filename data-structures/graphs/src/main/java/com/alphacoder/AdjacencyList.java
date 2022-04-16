fb320baa6d501ba0c72ece4f7cb0a5a9e0178f64
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-04-16 07:24:39 -0500

    Implemented bfs and shortest path.

--- data-structures/graphs/src/main/java/com/alphacoder/AdjacencyList.java
+++ data-structures/graphs/src/main/java/com/alphacoder/AdjacencyList.java
@@ -1,16 +1,21 @@
 package com.alphacoder;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class AdjacencyList {
     private int vertices;
     private LinkedList<Integer>[] edges;
     private boolean isDirected;
+    private int[] parents;
 
     public AdjacencyList(int vertices, boolean directed){
         this.vertices= vertices;
         edges= new LinkedList[vertices];
         this.isDirected= directed;
+        this.parents= new int[vertices];
     }
 
     /*
@@ -53,5 +58,50 @@ public class AdjacencyList {
             }
         }
         return list;
+    }
+
+    /*
+    Time complexity is O(V+M)
+     */
+    public void bfs(int start){
+        Queue<Integer> queue= new LinkedList<>();
+        boolean [] visited= new boolean[vertices];
+        queue.offer(start);
+        visited[start]= true;
+        parents[start]= -1;
+
+        while(!queue.isEmpty()){
+            int val= queue.poll();
+            System.out.print(val+ " ");
+            LinkedList<Integer> list= edges[val];
+            for(int i=0; i< list.size(); i++){
+                if(!visited[list.get(i)]) {
+                    queue.offer(list.get(i));
+                    visited[list.get(i)]= true;
+                    parents[list.get(i)]= val;
+                }
+            }
+        }
+    }
+
+    public int shortestPath(int source, int destination){
+        if(source== destination){
+            return 0;
+        }
+        List<Integer> path= new ArrayList<>();
+        int shortestPath= 1;
+        path.add(0, destination);
+        int temp= parents[destination];
+        while(temp!= source){
+            shortestPath++;
+            if(temp!= source) {
+                path.add(0, temp);
+            }
+            temp= parents[temp];
+        }
+
+        path.add(0, source);
+        path.stream().forEach(i -> System.out.print(i+ " "));
+        return shortestPath;
     }
 }
--- data-structures/graphs/src/main/java/com/alphacoder/AppMain.java
+++ data-structures/graphs/src/main/java/com/alphacoder/AppMain.java
@@ -2,6 +2,7 @@ package com.alphacoder;
 
 public class AppMain {
     public static void main(String[] args) {
+        System.out.println("***********AdjacencyMatrix**********************");
         AdjacencyMatrix graph = new AdjacencyMatrix(5);
         graph.addEdge(0, 1);
         graph.addEdge(0, 4);
@@ -21,5 +22,30 @@ public class AppMain {
         System.out.println();
 
         System.out.println("Shortest path: "+graph.shortestPath(0,4));
+
+        System.out.println("***********AdjacencyMatrix**********************");
+
+        System.out.println("***********AdjacencyList**********************");
+        AdjacencyList graphList = new AdjacencyList(5, false);
+        graphList.addEdge(0, 1);
+        graphList.addEdge(0, 4);
+        graphList.addEdge(1, 2);
+        graphList.addEdge(1, 3);
+        graphList.addEdge(1, 4);
+        graphList.addEdge(2, 3);
+        graphList.addEdge(3, 4);
+
+        graphList.deleteEdge(0, 4);
+        graphList.deleteEdge(1, 4);
+        graphList.deleteEdge(1, 3);
+
+
+        //adjacencyList bfs
+        graphList.bfs(0);
+        System.out.println();
+
+        System.out.println("Shortest path: "+graphList.shortestPath(0,4));
+
+        System.out.println("***********AdjacencyList**********************");
     }
 }
