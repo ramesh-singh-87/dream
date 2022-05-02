071da1e8359c3786767fe8e75b26c556de4d0643
Ramesh Singh <dream2big87@gmail.com>
2022-05-02 07:32:22 -0500

    Practicing BST problems.

--- data-structures/bst/binary-search-tree/src/main/java/com/alphacoder/BinarySearchTree.java
+++ data-structures/bst/binary-search-tree/src/main/java/com/alphacoder/BinarySearchTree.java
@@ -1,4 +1,33 @@
 package com.alphacoder;
 
 public class BinarySearchTree {
+    private Node root;
+
+    public void insert(int data){
+
+    }
+
+    public Node insertRec(Node root, int data){
+        if(root==null){
+            root= new Node(data);
+            return root;
+        }
+
+        if(data< root.data){
+            root.left= insertRec(root.left, data);
+        }else{
+            root.right= insertRec(root.right, data);
+        }
+
+        return root;
+    }
+
+    public void inOrder(Node root){
+        if(root== null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.data+ " ");
+        inOrder(root.right);
+    }
 }
