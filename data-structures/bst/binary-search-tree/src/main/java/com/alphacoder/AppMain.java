b9549b760daf0cedfab570eb4e5c24c3dcdd3835
Ramesh Singh <dream2big87@gmail.com>
2022-05-02 10:57:36 -0500

    Practicing BST problems.

--- data-structures/bst/binary-search-tree/src/main/java/com/alphacoder/AppMain.java
+++ data-structures/bst/binary-search-tree/src/main/java/com/alphacoder/AppMain.java
@@ -3,13 +3,28 @@ package com.alphacoder;
 public class AppMain {
     public static void main(String[] args) {
         BinarySearchTree bst= new BinarySearchTree();
-        bst.insert(3);
-        bst.insert(7);
-        bst.insert(8);
-        bst.insert(2);
-        bst.insert(0);
-        bst.insert(1);
+        Node root= null;
+        root= bst.insert(3);
+        root= bst.insert(7);
+        root= bst.insert(8);
+        root= bst.insert(2);
+        root= bst.insert(0);
+        root= bst.insert(1);
+        root= bst.insert(5);
 
+        bst.inOrder();
+        root= bst.deleteRec(7, root);
+
+        System.out.println();
+        bst.inOrder();
+        root= bst.deleteRec(5, root);
+
+        System.out.println();
+        bst.inOrder();
+
+        root= bst.deleteRec(8, root);
+
+        System.out.println();
         bst.inOrder();
     }
 }
--- data-structures/bst/binary-search-tree/src/main/java/com/alphacoder/BinarySearchTree.java
+++ data-structures/bst/binary-search-tree/src/main/java/com/alphacoder/BinarySearchTree.java
@@ -3,8 +3,9 @@ package com.alphacoder;
 public class BinarySearchTree {
     private Node root;
 
-    public void insert(int data){
+    public Node insert(int data){
         root=insertRec(root, data);
+        return root;
     }
 
     private Node insertRec(Node root, int data){
@@ -34,5 +35,46 @@ public class BinarySearchTree {
         inOrderRec(root.left);
         System.out.print(root.data+ " ");
         inOrderRec(root.right);
+    }
+
+
+
+    public Node deleteRec(int data, Node root){
+        if(root== null){
+            return root;
+        }
+
+        if(data< root.data){
+            root.left= deleteRec(data, root.left);
+        }else if(data> root.data){
+            root.right= deleteRec(data, root.right);
+        }else{
+            if(root.left== null && root.right== null){
+                root= null;
+            }
+            else if(root.left==null){
+                Node temp= root.right;
+                root.right= null;
+                root= temp;
+            }else if(root.right==null){
+                Node temp= root.left;
+                root.left= null;
+                root= temp;
+            }else{
+                Node temp= min(root.right);
+                root.data= temp.data;
+                root.right=deleteRec(temp.data, root.right);
+            }
+        }
+
+        return root;
+    }
+
+    private Node min(Node root){
+        while(root.left!= null){
+            root= root.left;
+        }
+
+        return root;
     }
 }
